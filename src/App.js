c27f024bcd76627a6124cfb2579a4476ca7c39d6
Guy Farley <guy.e.farley@gmail.com>
2022-05-16 17:13:59 -0700

    Added input form and table to display city data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+import Table from 'react-bootstrap/Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityLat: '',
+      cityLon: '',
+    }
+  }
+
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${this.state.city}&format=json`;
+    console.log(this.state.city);
+    let cityInfo = await axios.get(url);
+
+    console.log(cityInfo.data[0]);
+    console.log(cityInfo.data[0].lat);
+    console.log(cityInfo.data[0].lon);
+    this.setState({
+      cityName: cityInfo.data[0].display_name,
+      cityLat: cityInfo.data[0].lat,
+      cityLon: cityInfo.data[0].lon
+    });
+  }
+
+  cityChange = (e) => {
+    this.setState({
+      city: e.target.value
+    });
+  }
+
+
+  render() {
+
+    return (
+      <>
+        <form onSubmit={this.handleCitySubmit} >
+          <label htmlFor="cityName">Please Choose a City</label>
+          <input type="text" id="cityName" onChange={this.cityChange} />
+          <button type="submit">Explore!</button>
+        </form>
+
+        <Table
+          striped
+          bordered
+          hover>
+          <thead>
+            <tr>
+              <th>City Name</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{this.state.cityName}</td>
+              <td>{this.state.cityLat}</td>
+              <td>{this.state.cityLon}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </>
+    );
+  }
 }
 
 export default App;
