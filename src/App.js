1d198528e37f1bd895e63872f3125482ec2e1f4d
Guy Farley <guy.e.farley@gmail.com>
2022-05-18 17:33:29 -0700

    Added weather component to render forecast to page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Weather from './Weather.js'
 import axios from 'axios';
 import React from 'react';
 import Table from 'react-bootstrap/Table'
 import Figure from 'react-bootstrap/Figure'
+import Alert from 'react-bootstrap/Alert'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class App extends React.Component {
 
@@ -12,30 +18,41 @@ class App extends React.Component {
       city: '',
       cityLat: '',
       cityLon: '',
+      weatherData: [],
       showMap: false,
+      showTable: false,
       showWeather: false
     }
   }
 
   handleCitySubmit = async (e) => {
     e.preventDefault();
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${this.state.city}&format=json`;
-    let cityInfo = await axios.get(url);
+    try {
+      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${this.state.city}&format=json`;
+      let cityInfo = await axios.get(url);
 
-    // let weatherURL = `${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.city}`;
-    let weatherURL = `https://gf-city-explorer-301d85.herokuapp.com/weather?city_name=${this.state.city}`;
-    console.log(weatherURL);
-    let weather = await axios.get(weatherURL);
-    console.log(weather);
+      // let weatherURL = `${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.city}`;
+      let weatherURL = `https://gf-city-explorer-301d85.herokuapp.com/weather?city_name=${this.state.city}`;
 
-    this.setState({
-      cityName: cityInfo.data[0].display_name,
-      cityLat: cityInfo.data[0].lat,
-      cityLon: cityInfo.data[0].lon,
-      showMap: true,
-      showWeather: true
-    });
+      console.log(weatherURL);
+      let weather = await axios.get(weatherURL);
+      console.log(weather);
 
+      this.setState({
+        cityName: cityInfo.data[0].display_name,
+        cityLat: cityInfo.data[0].lat,
+        cityLon: cityInfo.data[0].lon,
+        weatherData: weather,
+        showMap: true,
+        showTable: true,
+        showWeather: true
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }
   }
 
   cityChange = (e) => {
@@ -44,49 +61,70 @@ class App extends React.Component {
     });
   }
 
-
   render() {
 
     return (
       <>
-        <form onSubmit={this.handleCitySubmit} >
-          <label htmlFor="cityName">Please Choose a City</label>
-          <input type="text" id="cityName" onChange={this.cityChange} />
-          <button type="submit">Explore!</button>
-        </form>
+        {this.state.error ?
+          <Alert key={'danger'} variant={'danger'}>
+            {this.state.errorMessage}
+          </Alert>
+          : null}
 
-        <Table
-          striped
-          bordered
-          hover>
-          <thead>
-            <tr>
-              <th>City Name</th>
-              <th>Latitude</th>
-              <th>Longitude</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{this.state.cityName}</td>
-              <td>{this.state.cityLat}</td>
-              <td>{this.state.cityLon}</td>
-            </tr>
-          </tbody>
-        </Table>
-        {this.state.showMap ?
-          <Figure>
-            <Figure.Image
-              width={400}
-              height={400}
-              alt="Map of {this.state.cityName}"
-              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_API_KEY}&center=${this.state.cityLat},${this.state.cityLon}&zoom=12`}
-            />
-            <Figure.Caption>
-              {this.state.cityName}
-            </Figure.Caption>
-          </Figure> : null}
+        <Form onSubmit={this.handleCitySubmit}>
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <Form.Label htmlFor="inputCity">Please enter a city name</Form.Label>
+              <Form.Control type="text" id="inputCity" onChange={this.cityChange} />
+              <Button variant="primary" type="submit">Explore!</Button>
+            </Col>
+          </Row>
+        </Form>
 
+        {
+          this.state.showTable ?
+            <Table
+              striped
+              bordered
+              hover>
+              <thead>
+                <tr>
+                  <th>City Name</th>
+                  <th>Latitude</th>
+                  <th>Longitude</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{this.state.cityName}</td>
+                  <td>{this.state.cityLat}</td>
+                  <td>{this.state.cityLon}</td>
+                </tr>
+              </tbody>
+            </Table> : null
+        }
+        {
+          this.state.showMap ?
+            <Figure>
+              <Figure.Image
+                width={400}
+                height={400}
+                alt="Map of {this.state.cityName}"
+                src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_API_KEY}&center=${this.state.cityLat},${this.state.cityLon}&zoom=12`}
+              />
+              <Figure.Caption>
+                {this.state.cityName}
+              </Figure.Caption>
+            </Figure> : null
+        }
+        {
+          this.state.showWeather ?
+            <Weather
+              city={this.state.city}
+              cityLat={this.state.cityLat}
+              cityLon={this.state.cityLon}
+              weather={this.state.weatherData}
+            /> : null}
       </>
     );
   }
---
+++ src/Weather.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React from 'react';
+import Table from 'react-bootstrap/Table'
+
+class Weather extends React.Component {
+
+
+  render() {
+
+    return (
+
+      <Table
+        striped
+        bordered
+        hover>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Forecast</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{this.props.weather.data[0].date}</td>
+            <td>{this.props.weather.data[0].description}</td>
+          </tr>
+          <tr>
+            <td>{this.props.weather.data[1].date}</td>
+            <td>{this.props.weather.data[1].description}</td>
+          </tr>
+          <tr>
+            <td>{this.props.weather.data[2].date}</td>
+            <td>{this.props.weather.data[2].description}</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
+  }
+}
+
+
+export default Weather;
